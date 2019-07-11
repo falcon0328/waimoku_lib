f29c61aa117a2c6e7d6a3660f75909fe75516939
aseo <aseo@yahoo-corp.jp>
2019-07-11 22:35:18 +0900

    feat: 外から, WaimokuUserListProviderを呼べるように修正

--- waimoku/__init__.py
+++ waimoku/__init__.py
@@ -1,3 +1,4 @@
 from .user import WaimokuUser
 from .participationn_status import WaimokuJoinStatus
 from .setsuei_status import WaimokuSetsueiStatus
+from .user_list_provider import WaimokuUserListProvider
