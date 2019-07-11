f1b83c2d7110d2dd988c6a01bc66ea573222e7de
aseo <aseo@yahoo-corp.jp>
2019-07-11 22:36:03 +0900

    feat: ユーザ一覧を取得する処理（文字列の部分のみ）を実装

--- waimoku/user_list_provider.py
+++ waimoku/user_list_provider.py
@@ -1,2 +1,29 @@
+import pandas as pd
+from .user import WaimokuUser
+from .participationn_status import WaimokuJoinStatus
+from .setsuei_status import WaimokuSetsueiStatus
+
+
 class WaimokuUserListProvider:
-    pass
+    raw_data: pd.DataFrame
+
+    def fetch(self, file_path: str, encoding: str = "cp932") -> list:
+        user_list = []
+        """指定されたファイルパスに存在するファイルからユーザ一覧を取得する
+
+        Arguments:
+            file_path {str} -- ファイルパス
+        """
+        self.raw_data = pd.read_csv(file_path, encoding=encoding)
+        for data in self.raw_data.itertuples():
+            user = WaimokuUser(user_name=data[2],
+                               display_name=data[3],
+                               join_status=None,
+                               participation_status=None,
+                               mokumoku=data[9],
+                               is_handagote=True,
+                               is_enquete=True,
+                               is_setsuei=None,
+                               is_lt=False)
+            user_list.append(user)
+        return user_list
