87e599acc97b2c0e85fab266b454d7e7acdc1392
aseo <aseo@yahoo-corp.jp>
2019-07-15 17:44:32 +0900

    feat: Excelファイルにユーザの一覧を保存する処理を実装

--- waimoku/client.py
+++ waimoku/client.py
@@ -1,2 +1,37 @@
+import openpyxl as excel
+
+from . import WaimokuUser
+
+
 class WaimokuClient:
-    pass
+    __headerTitles: dict = {"A2": "通し番号", "B2": "区分", "C2": "氏名", "D2": "所属"}
+    __cellWidths: list = [6.83, 4.83, 17.33, 44.67]
+
+    def save_to_file(self, user_list: [WaimokuUser], save_filename="event_participantsList.xlsx"):
+        # ブックの新規作成
+        wb = excel.Workbook()
+        # アクティブなシートの作成
+        ws = wb.active
+        # シートタイトルはSheet1に変更
+        ws.title = "Sheet1"
+        # ヘッダーを用意
+        for key in self.__headerTitles.keys():
+            ws[key] = self.__headerTitles[key]
+        for index, user in enumerate(user_list):
+            ws["A{0}".format(index + 3)] = index + 1
+            ws["C{0}".format(index + 3)] = user.full_name
+            ws["D{0}".format(index + 3)] = user.assign
+        # シートの幅を調節する
+        self.__adjust_width(ws)
+        # ファイル保存
+        wb.save(save_filename)
+
+    def __adjust_width(self, ws):
+        """指定したシートの幅を調節する
+
+        Arguments:
+            ws {[]} -- 対象のシート
+        """
+        for index, col in enumerate(self.__cellWidths):
+            ws.column_dimensions[excel.utils.get_column_letter(index + 1)].width = self.__cellWidths[index]
+        return ws
