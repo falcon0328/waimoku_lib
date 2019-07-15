d02f4a0f64762c027ef3dd1b67e571d3577282c0
aseo <aseo@yahoo-corp.jp>
2019-07-15 14:44:07 +0900

    refactor: 不必要な構造体の削除

--- waimoku/__init__.py
+++ waimoku/__init__.py
@@ -1,4 +1,3 @@
 from .user import WaimokuUser
-from .participationn_status import WaimokuJoinStatus
 from .setsuei_status import WaimokuSetsueiStatus
 from .user_list_provider import WaimokuUserListProvider
