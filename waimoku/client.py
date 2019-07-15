42e0889bd4b3e487ff3d27b3194bd2dd355d601c
aseo <aseo@yahoo-corp.jp>
2019-07-15 23:11:30 +0900

    feat: 罫線を引く処理を追加

--- waimoku/client.py
+++ waimoku/client.py
@@ -3,6 +3,7 @@ from openpyxl.styles.fonts import Font
 from openpyxl.utils import get_column_letter
 from openpyxl.styles.alignment import Alignment
 from openpyxl.styles import PatternFill
+from openpyxl.styles.borders import Border, Side
 from . import WaimokuUser
 
 
@@ -13,6 +14,13 @@ class WaimokuClient:
     __header_collor: PatternFill = PatternFill(patternType='solid', fgColor="FFF2CC")
     __font_sizes: list = [9, 9, 11, 11]
     __cellWidths: list = [6.83, 4.83, 17.33, 44.67]
+    __cell_side = Side(style='thin', color='000000')
+    __cell_border: Border
+
+    def __init__(self, *args, **kwargs):
+        Border()
+        self.__cell_border = Border(top=self.__cell_side, bottom=self.__cell_side, left=self.__cell_side, right=self.__cell_side)
+        return super().__init__(*args, **kwargs)
 
     def save_to_file(self, user_list: [WaimokuUser], save_filename="event_participantsList.xlsx"):
         # ブックの新規作成
@@ -35,11 +43,11 @@ class WaimokuClient:
         self.__adjust_sheet_alignment(ws)
         # 「入力必須」の表示を行うセルを用意
         # B1 ~ D1のセルを連結
-        self.__merge_cells_and_setting_to_align(ws=ws, key=self.__merge_target_cell, merge_cells=self.__merge_target_cells, value="入力必須", fill=self.__header_collor)
+        self.__merge_cells_and_setting_to_align(ws=ws, key=self.__merge_target_cell, merge_cells=self.__merge_target_cells, value="入力必須", fill=self.__header_collor, border=self.__cell_border)
         # ファイル保存
         wb.save(save_filename)
 
-    def write(self, ws, key: str, value, fill: PatternFill = PatternFill(), font_size: int = 9):
+    def write(self, ws, key: str, value, fill: PatternFill = PatternFill(), border=Border(top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'), left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000')), font_size: int = 9):
         """ワークシートの指定したキーにデータを書き込む
 
         Arguments:
@@ -49,10 +57,12 @@ class WaimokuClient:
 
         Keyword Arguments:
             fill {PatternFill} -- 色の指定 (default: {PatternFill()})
+            border {Border} -- 罫線情報(default: {Border(top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'), left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'))})
             font_size {int} -- フォントサイズ (default: {9})
         """
         ws[key] = value
         ws[key].fill = fill
+        ws[key].border = border
         ws[key].font = Font(size=font_size)
 
     def __adjust_sheet_size(self, ws):
@@ -79,7 +89,7 @@ class WaimokuClient:
             for cell in row:
                 cell.alignment = align
 
-    def __merge_cells_and_setting_to_align(self, ws, key: str, merge_cells: str, value, fill: PatternFill = PatternFill(), align: Alignment = Alignment(horizontal="center", vertical="center")):
+    def __merge_cells_and_setting_to_align(self, ws, key: str, merge_cells: str, value, fill: PatternFill = PatternFill(), align: Alignment = Alignment(horizontal="center", vertical="center"), border=Border(top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'), left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'))):
         """指定したセル達を連結後に値をセットし、アラインと色を適用する
 
         Arguments:
@@ -91,8 +101,10 @@ class WaimokuClient:
         Keyword Arguments:
             fill {PatternFill} -- 色の指定 (default: {PatternFill()})
             align {Alignment} -- アライン情報 (default: {Alignment(horizontal="center", vertical="center")})
+            border {Border} -- 罫線情報(default: {Border(top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'), left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'))})
         """
         ws[key] = value
+        ws[key].border = border
         ws.merge_cells(merge_cells)
         ws[key].fill = fill
         ws[key].alignment = align
