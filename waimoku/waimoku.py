b7a2cd0d2863e516e156b40e25efe7d1b0ba170c
aseo <aseo@yahoo-corp.jp>
2019-07-16 22:09:16 +0900

    feat: print出力を行うように変更

--- waimoku/waimoku.py
+++ waimoku/waimoku.py
@@ -27,34 +27,35 @@ class Waimoku:
         """
         return self.__user_list_provider.fetch(file_path=file_path)
 
-    def save_to_file_from_csv_file(self, file_path: str, save_filename: str, is_participation: bool = True):
+    def save_to_file_from_csv_file(self, file_path: str, save_filename: str):
         """Connpassの参加者一覧CSVファイルからLODGE提出用のXLSファイルに変換して保存する
 
         Arguments:
             file_path {str} -- ConnpassのCSVファイルが存在するパス
             save_filename {str} -- LODGE提出用のXLSファイル
-
-        Keyword Arguments:
-            is_participation {bool} -- ファイルに保存するのを参加したユーザだけにするかどうか (default: {True})
         """
         user_list = self.fetch_users(file_path=file_path)
-        self.save_to_file_user_list(user_list=user_list, save_filename=save_filename, is_participation=is_participation)
+        self.save_to_file_user_list(user_list=user_list, save_filename=save_filename)
 
-    def save_to_file_user_list(self, user_list: [WaimokuUser], save_filename: str, is_participation: bool = True):
+    def save_to_file_user_list(self, user_list: [WaimokuUser], save_filename: str):
         """ワイもくのユーザ情報一覧をLODGE提出用のXLSファイルに変換して保存する
 
         Arguments:
             user_list {[WaimokuUser]} -- ワイもくのユーザ情報一覧
             save_filename {str} -- LODGE提出用のXLSファイル
-
-        Keyword Arguments:
-            is_participation {bool} -- ファイルに保存するのを参加したユーザだけにするかどうか (default: {True})
         """
+        is_yahoo_count = 0
+        is_unei_count = 0
         participation_list: [WaimokuUser] = []
-        if is_participation:
-            for user in user_list:
-                if user.participation_status:
-                    participation_list.append(user)
-        else:
-            participation_list = user_list
+        for user in user_list:
+            if user.participation_status:
+                participation_list.append(user)
+            if user.is_yahoo:
+                is_yahoo_count = is_yahoo_count + 1
         self.__client.save_to_file(user_list=participation_list, save_filename=save_filename)
+        print("")
+        print("=== ヤフー社員合計 ===")
+        print("ヤフ-社員: " + str(is_yahoo_count) + "名")
+        print("=== 参加者数（この中にはヤフー社員も含めます） ===")
+        print("運営: " + str(is_unei_count) + "名")
+        print("一般: " + str(len(participation_list) - is_unei_count))
