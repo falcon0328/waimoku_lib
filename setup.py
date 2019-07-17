ac21a89eb711fb24210f209f224e68313fecba69
aseo <aseo@yahoo-corp.jp>
2019-07-17 22:35:32 +0900

    chore: setup.pyの階層を修正

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup
 from setuptools import find_packages
+
+print(find_packages())
 setup(
     name='waimoku',
     version='0.0.1',
     description='PythonScript to convert "Waimoku" participant information into "Yahoo! LODGE" participant list file format',
     author='Atsuki Seo',
     install_requires=["pandas", "openpyxl"],
+    packages=find_packages(),
     package_data={
         "waimoku": ["res/*.xlsx"]
+    },
+    entry_points={
+        'console_scripts': [
+            'myapp = waimoku.main:main',
+        ],
     },
 )
